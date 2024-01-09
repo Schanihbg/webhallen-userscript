02f00e5141420617acc27bebfc179b363d1f2c28
Joakim Hedlund <contact@joakimhedlund.com>
2024-01-09 21:45:43 +0100

    fix: use await syntax rather than noop-catch when possible

--- src/userscript.ts
+++ src/userscript.ts
@@ -8,14 +8,13 @@ const doRouting = async (): Promise<void> => {
   const { pathname } = document.location
   if (pathname.startsWith('/se/member')) {
     console.log('On member pages. Requesting user info and appending stats link.')
-    fetchMe()
+    await fetchMe()
       .then((user) => {
         if (!user) return
         setCachedUser(user)
 
         addStatisticsLink()
       })
-      .catch(() => {})
   }
 }
 
