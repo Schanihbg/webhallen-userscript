85551c0f6f2a5770dfa65d56166d989c9351a0b4
Joakim Hedlund <contact@joakimhedlund.com>
2024-01-09 20:24:20 +0100

    feat: automatic updates 🙏

--- userscript-header.js
+++ userscript-header.js
@@ -6,6 +6,8 @@
 // @author       $USER_SCRIPT_AUTHORS
 // @match        https://www.webhallen.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=webhallen.com
+// @updateURL    https://github.com/Schanihbg/webhallen-sum-userscript/releases/latest/download/webhallen.user.js
+// @downloadURL  https://github.com/Schanihbg/webhallen-sum-userscript/releases/latest/download/webhallen.user.js
 // @grant        GM_addStyle
 // @grant        window.onurlchange
 // ==/UserScript==
