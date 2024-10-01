fe66f63e94c5107c1cdc4d387b7806c1d641319d
Furiiku <github@freakhouse.se>
2024-10-01 21:24:39 +0200

    fix: make sort more readable

--- src/reducers/reviews.ts
+++ src/reducers/reviews.ts
@@ -2,7 +2,12 @@ import { type ProductReview } from '../lib/api'
 
 export function getPostedReviews (reviews: ProductReview[]): ProductReview[] {
   return reviews.filter(orderReview => orderReview.review !== undefined)
-    .sort((a, b) => (b.review!.createdAt - a.review!.createdAt));
+    .sort((a, b) => {
+      if (a.review && b.review) {
+        return b.review.createdAt - a.review.createdAt
+      }
+      return 0
+    })
 }
 
 export function getProductsWithoutReviews (reviews: ProductReview[]): ProductReview[] {
